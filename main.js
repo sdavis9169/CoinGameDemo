3cddc4a8e5fd22d825b6daf80c04b675750278ca
Sdavis <stevendavis0622@gmail.com>
2020-06-13 21:32:42 -0600

    Coin game first commit

--- main.js
+++ main.js
@@ -1,29 +1,47 @@
-const colors = [
-  `red`,
-  `orange`,
-  `yellow`,
-  `green`,
-  `blue`,
-  `purple`,
-  `indigo`,
-  `violet`
-];
+function isTouching(a, b) {
+  const aRect = a.getBoundingClientRect();
+  const bRect = b.getBoundingClientRect();
 
-const changeColor = function() {
-  const h1 = document.querySelector('h1');
-  h1.style.color = this.style.backgroundColor;
-};
-
-const printColor = function() {
-  console.log(this.style.backgroundColor);
-};
-
-const container = document.querySelector('#boxes');
-
-for (let color of colors) {
-  const box = document.createElement('div');
-  box.style.backgroundColor = color;
-  box.classList.add('box');
-  container.append(box);
-  box.addEventListener('click', changeColor);
+  return !(
+    aRect.top + aRect.height < bRect.top ||
+    aRect.top > bRect.top + bRect.height ||
+    aRect.left + aRect.width < bRect.left ||
+    aRect.left > bRect.left + bRect.width
+  );
 }
+
+const avatar = document.querySelector('#player');
+const coin = document.querySelector('#coin');
+
+window.addEventListener('keyup', function(e) {
+  if (e.key === 'ArrowDown' || e.key === 'Down') {
+    const currTop = extractPosition(avatar.style.top);
+    avatar.style.top = `${currTop + 50}px`;
+  } else if (e.key === 'ArrowUp' || e.key === 'Up') {
+    const currTop = extractPosition(avatar.style.top);
+    avatar.style.top = `${currTop - 50}px`;
+  } else if (e.key === 'ArrowRight' || e.key === 'Right') {
+    const currLeft = extractPosition(avatar.style.left);
+    avatar.style.left = `${currLeft + 50}px`;
+    avatar.style.transform = 'scale(1,1)';
+  } else if (e.key === 'ArrowLeft' || e.key === 'Left') {
+    const currLeft = extractPosition(avatar.style.left);
+    avatar.style.left = `${currLeft - 50}px`;
+    avatar.style.transform = 'scale(-1,1)';
+  }
+  if (isTouching(avatar, coin)) moveCoin();
+});
+
+const moveCoin = () => {
+  const x = Math.floor(Math.random() * window.innerHeight);
+  const y = Math.floor(Math.random() * window.innerWidth);
+  coin.style.top = `${y}px`;
+  coin.style.left = `${x}px`;
+};
+
+const extractPosition = pos => {
+  if (!pos) return 100;
+  return parseInt(pos.slice(0, -2));
+};
+
+moveCoin();
