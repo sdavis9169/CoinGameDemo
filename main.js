91976f0c3499b6ead50f3ac4c964b4bd2c85a561
Sdavis <stevendavis0622@gmail.com>
2020-06-13 20:07:36 -0600

    Just for fun, color changer

---
+++ main.js
@@ -0,0 +1,29 @@
+const colors = [
+  `red`,
+  `orange`,
+  `yellow`,
+  `green`,
+  `blue`,
+  `purple`,
+  `indigo`,
+  `violet`
+];
+
+const changeColor = function() {
+  const h1 = document.querySelector('h1');
+  h1.style.color = this.style.backgroundColor;
+};
+
+const printColor = function() {
+  console.log(this.style.backgroundColor);
+};
+
+const container = document.querySelector('#boxes');
+
+for (let color of colors) {
+  const box = document.createElement('div');
+  box.style.backgroundColor = color;
+  box.classList.add('box');
+  container.append(box);
+  box.addEventListener('click', changeColor);
+}
